d2b65679bc34af1fda93096a194074061058d9b0
nbhat1 <neeraj.engg@gmail.com>
2016-12-30 16:27:30 +0530

    New gitignore file & updated Multiple Dropdown class file

--- src/main/MultipleDropDown.java
+++ src/main/MultipleDropDown.java
@@ -18,13 +18,34 @@ public class MultipleDropDown {
         driver.get("http://www.wikipedia.org");
 
         Select select = new Select(driver.findElement(By.xpath(".//*[@id='searchLanguage']")));
-        List<WebElement> options = driver.findElements(By.tagName("option"));
+        System.out.println("************************Printing All Dropdown values****************************************");
+          /*List<WebElement> options = driver.findElements(By.tagName("option"));
         System.out.println("Size of list is" + "" + options.size()+ "" + "Elements");
-        //System.out.println(options.);
-        // int i;
-        for(int i = 0; i<options.size(); i++){
+        for(int i = 0; i<options.size(); i++) {
             System.out.println(options.get(i).getText());
+            System.out.println(options.get(i).getAttribute("value"));
+        }*/
 
+            System.out.println("****************************Print All links on page*************************************");
 
+            List<WebElement> links = driver.findElements(By.tagName("a"));
+
+            for (int j=0;j<links.size();j++){
+                System.out.println(links.get(j).getAttribute("href"));
+                System.out.println(links.get(j).getText());
+            }
+        System.out.println("*********************Total Links are**************** " + "" + links.size());
+
+        /*System.out.println("*********************************Print links from a block on webpage****************************");
+        List<WebElement> block = driver.findElements(By.xpath("./*//*[@class='footer-sidebar-text']/ul"));
+        System.out.println(block.size());
+        for (int k=0; k<block.size();k++){
+            System.out.println(block.get(k).getAttribute("href"));
+            System.out.println(block.get(k).getText());
+        }
+        System.out.println("Total Links in block :" + "" + block.size());
+*/
+        //driver.quit();
     }
-} }
+    }
+
